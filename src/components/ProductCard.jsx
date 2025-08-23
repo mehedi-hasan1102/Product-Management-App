da76c7355fc8454aa90cd772cdd635913a14543e
Mehedi Hasan <mehedi.hasan11023@gmail.com>
2025-08-23 06:20:36 +0600

    (feat) i update readme

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({ product }) => {
         <h2 className="text-xl font-bold mb-2">{product.name}</h2>
         <p className="text-gray-700 mb-4">{product.description}</p>
         <p className="text-gray-900 font-bold text-lg">{product.price}</p>
-        <Link href={`/products/${product._id}`} className="mt-4 inline-block bg-gradient-to-r from-emerald-500 to-cyan-600  text-gray-900 px-4 py-2 rounded hover:opacity-90 transition">
+        <Link href={`/products/${product._id}`} className="mt-4 inline-block bg-gradient-to-r from-emerald-500 to-cyan-600   px-4 py-2 rounded hover:opacity-90 transition text-white">
           Details
         </Link>
       </div>
