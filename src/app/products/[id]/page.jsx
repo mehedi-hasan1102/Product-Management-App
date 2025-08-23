82a125f4d13e396e4010d27ff72a04618bf1a7d5
Mehedi Hasan <mehedi.hasan11023@gmail.com>
2025-08-23 06:14:14 +0600

    (feat) i update

--- src/app/products/[id]/page.jsx
+++ src/app/products/[id]/page.jsx
@@ -87,7 +87,7 @@ const ProductDetailsPage = () => {
       <p className="text-3xl font-bold text-indigo-600">{product.price}</p>
 
       <div className="flex gap-4 mt-6">
-        <button className="px-6 py-3 bg-gradient-to-r from-emerald-500 to-cyan-600  text-white font-medium rounded-xl shadow hover:bg-indigo-700 transition">
+        <button className="px-6 py-3 bg-gradient-to-r from-emerald-500 to-cyan-600  text-white font-medium rounded-xl shadow hover:opacity-90  transition">
           Buy Now
         </button>
         <button className="px-6 py-3 border border-gray-300 text-gray-700 font-medium rounded-xl hover:bg-gray-100 transition">
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({ product }) => {
         <h2 className="text-xl font-bold mb-2">{product.name}</h2>
         <p className="text-gray-700 mb-4">{product.description}</p>
         <p className="text-gray-900 font-bold text-lg">{product.price}</p>
-        <Link href={`/products/${product._id}`} className="mt-4 inline-block bg-yellow-400 text-gray-900 px-4 py-2 rounded hover:bg-yellow-500">
+        <Link href={`/products/${product._id}`} className="mt-4 inline-block bg-gradient-to-r from-emerald-500 to-cyan-600  text-gray-900 px-4 py-2 rounded hover:opacity-90 transition">
           Details
         </Link>
       </div>
